90173bf9ee395e283bc85e22b880e40c564c12ef
Moinul Islam <moinulilm10@gmail.com>
2025-04-10 10:50:48 +0600

    `Added routes for company subdomains with admin prefix and auth middleware`

--- routes/web.php
+++ routes/web.php
@@ -25,3 +25,27 @@ Route::name('admin.')->prefix('admin')->group(function () {
     Route::get('/posts', [PostController::class, 'index'])->name('posts.index');
     Route::get('/posts/{id}', [PostController::class, 'show'])->name('posts.show');
 });
+
+
+
+
+// group routes
+Route::domain('{company}.myblog.com')->group(function () {
+
+    // Admin routes with auth middleware, prefix and naming
+    Route::prefix('admin')
+        ->name('admin.')
+        ->middleware('auth')
+        ->group(function () {
+
+            // Grouped controller routes
+            Route::controller(PostController::class)->group(function () {
+                Route::get('/posts', 'index')->name('posts.index');
+                Route::post('/posts', 'store')->name('posts.store');
+            });
+
+            Route::controller(UserController::class)->group(function () {
+                Route::get('/users', 'index')->name('users.index');
+            });
+        });
+});
